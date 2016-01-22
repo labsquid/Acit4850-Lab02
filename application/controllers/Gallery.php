a6bbccaf167a4e3003e525e432c97e3ed3390f66
labsquid <labsquidthrowaway@gmail.com>
2016-01-22 13:44:39 -0800

    Recommiting Lab 3, gallery now function using database for images

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -20,6 +20,27 @@ class Gallery extends Application {
 	 */
 	public function index()
 	{
+		//get all images from our model
+		$pix = $this->images->all();
+		
+		//build an array of formatted cells for them
+		foreach ($pix as $picture)
+			$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+		
+		//prime the table class
+		$this->load->library('table');
+		$parms = array(
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td class="oneimages">'
+		);
+		$this->table->set_template($parms);
+		
+		//generate table
+		$rows = $this->table->make_columns($cells, 3);
+		$this->data['thetable'] = $this->table->generate($rows);
+		
+		
 		//$this->load->view('gallery');
 		$this->data['pagebody'] = 'gallery';
 		$this->render();
